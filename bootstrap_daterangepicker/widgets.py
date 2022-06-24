06c7ac7d4b823cffc36e6629f68637cc2bab23a9
evripidisvak <ev.vakalis@gmail.com>
2022-06-24 13:10:09 +0300

    Update widgets.py

--- bootstrap_daterangepicker/widgets.py
+++ bootstrap_daterangepicker/widgets.py
@@ -8,6 +8,7 @@ from django import forms
 from django.utils import formats
 from django.utils.safestring import mark_safe
 from django.utils.translation import gettext as _
+from django.core.serializers.json import DjangoJSONEncoder
 
 __all__ = ['DatePickerWidget', 'DateRangeWidget', 'DateTimeRangeWidget', 'add_month', 'common_dates']
 
