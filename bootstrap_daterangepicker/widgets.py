e776e5966bd2abc9422dc7154a60e76c9dca7843
evripidisvak <ev.vakalis@gmail.com>
2022-10-17 10:25:24 +0300

    Update widgets.py

--- bootstrap_daterangepicker/widgets.py
+++ bootstrap_daterangepicker/widgets.py
@@ -10,23 +10,29 @@ from django.utils.safestring import mark_safe
 from django.utils.translation import gettext as _
 from django.core.serializers.json import DjangoJSONEncoder
 
-__all__ = ['DatePickerWidget', 'DateRangeWidget', 'DateTimeRangeWidget', 'add_month', 'common_dates']
+__all__ = [
+    "DatePickerWidget",
+    "DateRangeWidget",
+    "DateTimeRangeWidget",
+    "add_month",
+    "common_dates",
+]
 
 format_to_js = {
-    '%m': 'MM',
-    '%d': 'DD',
-    '%Y': 'YYYY',
-    '%y': 'YY',
-    '%B': 'MMMM',
-    '%b': 'MMM',
-    '%M': 'mm',
-    '%H': 'HH',
-    '%I': 'hh',
-    '%p': 'A',
-    '%S': 'ss',
-    }
+    "%m": "MM",
+    "%d": "DD",
+    "%Y": "YYYY",
+    "%y": "YY",
+    "%B": "MMMM",
+    "%b": "MMM",
+    "%M": "mm",
+    "%H": "HH",
+    "%I": "hh",
+    "%p": "A",
+    "%S": "ss",
+}
 
-format_to_js_re = re.compile(r'(?<!\w)(' + '|'.join(format_to_js.keys()) + r')\b')
+format_to_js_re = re.compile(r"(?<!\w)(" + "|".join(format_to_js.keys()) + r")\b")
 
 
 def add_month(start_date, months):
@@ -35,25 +41,194 @@ def add_month(start_date, months):
 
 def common_dates(date_format, start_date=date.today()):
     one_day = timedelta(days=1)
-    return OrderedDict([
-        ('Today', (start_date.strftime(date_format), start_date.strftime(date_format))),
-        ('Yesterday', ((start_date - one_day).strftime(date_format), (start_date - one_day).strftime(date_format))),
-        ('This week', ((start_date - timedelta(days=start_date.weekday())).strftime(date_format), start_date.strftime(date_format))),
-        ('Last week', ((start_date - timedelta(days=start_date.weekday() + 7)).strftime(date_format),
-                    (start_date - timedelta(days=start_date.weekday() + 1)).strftime(date_format))),
-        ('Week ago', ((start_date - timedelta(days=7)).strftime(date_format), start_date.strftime(date_format))),
-        ('This month', ((start_date.replace(day=1)).strftime(date_format), start_date.strftime(date_format))),
-        ('Last month', ((add_month(start_date.replace(day=1), -1)).strftime(date_format), (start_date.replace(day=1) - one_day).strftime(date_format))),
-        ('3 months', ((add_month(start_date, -3)).strftime(date_format), start_date.strftime(date_format))),
-        ('Year', ((add_month(start_date, -12)).strftime(date_format), start_date.strftime(date_format))),
-        ])
+    return OrderedDict(
+        [
+            (
+                "Today",
+                (start_date.strftime(date_format), start_date.strftime(date_format)),
+            ),
+            (
+                "Yesterday",
+                (
+                    (start_date - one_day).strftime(date_format),
+                    (start_date - one_day).strftime(date_format),
+                ),
+            ),
+            (
+                "This week",
+                (
+                    (start_date - timedelta(days=start_date.weekday())).strftime(
+                        date_format
+                    ),
+                    start_date.strftime(date_format),
+                ),
+            ),
+            (
+                "Last week",
+                (
+                    (start_date - timedelta(days=start_date.weekday() + 7)).strftime(
+                        date_format
+                    ),
+                    (start_date - timedelta(days=start_date.weekday() + 1)).strftime(
+                        date_format
+                    ),
+                ),
+            ),
+            (
+                "Week ago",
+                (
+                    (start_date - timedelta(days=7)).strftime(date_format),
+                    start_date.strftime(date_format),
+                ),
+            ),
+            (
+                "This month",
+                (
+                    (start_date.replace(day=1)).strftime(date_format),
+                    start_date.strftime(date_format),
+                ),
+            ),
+            (
+                "Last month",
+                (
+                    (add_month(start_date.replace(day=1), -1)).strftime(date_format),
+                    (start_date.replace(day=1) - one_day).strftime(date_format),
+                ),
+            ),
+            (
+                "3 months",
+                (
+                    (add_month(start_date, -3)).strftime(date_format),
+                    start_date.strftime(date_format),
+                ),
+            ),
+            (
+                "Year",
+                (
+                    (add_month(start_date, -12)).strftime(date_format),
+                    start_date.strftime(date_format),
+                ),
+            ),
+        ]
+    )
+
+
+def common_datetimes(date_format, start_date=date.today()):
+    one_day = timedelta(days=1)
+    return OrderedDict(
+        [
+            (
+                "Today",
+                (
+                    start_date.strftime(date_format),
+                    start_date.strftime(date_format).replace(
+                        hour=23, minute=59, second=59, microsecond=999999
+                    ),
+                ),
+            ),
+            (
+                "Yesterday",
+                (
+                    (start_date - one_day).strftime(date_format),
+                    (start_date - one_day)
+                    .strftime(date_format)
+                    .replace(hour=23, minute=59, second=59, microsecond=999999),
+                ),
+            ),
+            (
+                "Yesterday evening",
+                (
+                    (start_date - one_day)
+                    .strftime(date_format)
+                    .replace(hour=18, minute=00, second=00, microsecond=0),
+                    (start_date - one_day)
+                    .strftime(date_format)
+                    .replace(hour=20, minute=59, second=59, microsecond=999999),
+                ),
+            ),
+            (
+                "This week",
+                (
+                    (start_date - timedelta(days=start_date.weekday())).strftime(
+                        date_format
+                    ),
+                    start_date.strftime(date_format).replace(
+                        hour=23, minute=59, second=59, microsecond=999999
+                    ),
+                ),
+            ),
+            (
+                "Last week",
+                (
+                    (start_date - timedelta(days=start_date.weekday() + 7)).strftime(
+                        date_format
+                    ),
+                    (start_date - timedelta(days=start_date.weekday() + 1))
+                    .strftime(date_format)
+                    .replace(hour=23, minute=59, second=59, microsecond=999999),
+                ),
+            ),
+            (
+                "Week ago",
+                (
+                    (start_date - timedelta(days=7)).strftime(date_format),
+                    start_date.strftime(date_format).replace(
+                        hour=23, minute=59, second=59, microsecond=999999
+                    ),
+                ),
+            ),
+            (
+                "This month",
+                (
+                    (start_date.replace(day=1)).strftime(date_format),
+                    start_date.strftime(date_format).replace(
+                        hour=23, minute=59, second=59, microsecond=999999
+                    ),
+                ),
+            ),
+            (
+                "Last month",
+                (
+                    (add_month(start_date.replace(day=1), -1)).strftime(date_format),
+                    (start_date.replace(day=1) - one_day)
+                    .strftime(date_format)
+                    .replace(hour=23, minute=59, second=59, microsecond=999999),
+                ),
+            ),
+            (
+                "3 months",
+                (
+                    (add_month(start_date, -3)).strftime(date_format),
+                    start_date.strftime(date_format).replace(
+                        hour=23, minute=59, second=59, microsecond=999999
+                    ),
+                ),
+            ),
+            (
+                "Year",
+                (
+                    (add_month(start_date, -12)).strftime(date_format),
+                    start_date.strftime(date_format).replace(
+                        hour=23, minute=59, second=59, microsecond=999999
+                    ),
+                ),
+            ),
+        ]
+    )
 
 
 class DateRangeWidget(forms.TextInput):
-    format_key = 'DATE_INPUT_FORMATS'
-    template_name = 'bootstrap_daterangepicker/daterangepicker.html'
+    format_key = "DATE_INPUT_FORMATS"
+    template_name = "bootstrap_daterangepicker/daterangepicker.html"
 
-    def __init__(self, picker_options=None, attrs=None, format=None, separator=' - ', clearable=False):
+    def __init__(
+        self,
+        picker_options=None,
+        attrs=None,
+        format=None,
+        separator=" - ",
+        clearable=False,
+    ):
         super(DateRangeWidget, self).__init__(attrs)
 
         self.separator = separator
@@ -61,8 +236,8 @@ class DateRangeWidget(forms.TextInput):
         self.picker_options = picker_options or {}
         self.clearable_override = clearable
 
-        if 'class' not in self.attrs:
-            self.attrs['class'] = 'form-control'
+        if "class" not in self.attrs:
+            self.attrs["class"] = "form-control"
 
     def clearable(self):
         """clearable if the field is an optional field or if explicitly set as clearable"""
@@ -78,64 +253,68 @@ class DateRangeWidget(forms.TextInput):
 
     def format_value(self, value):
         if isinstance(value, tuple):
-            return self._format_date_value(value[0]) + \
-                   self.separator + \
-                   self._format_date_value(value[1])
+            return (
+                self._format_date_value(value[0])
+                + self.separator
+                + self._format_date_value(value[1])
+            )
         else:
             return value
 
     def get_context(self, name, value, attrs):
         context = super().get_context(name, value, attrs)
-        date_format = format_to_js_re.sub(lambda m: format_to_js[m.group()], self._get_format())
+        date_format = format_to_js_re.sub(
+            lambda m: format_to_js[m.group()], self._get_format()
+        )
 
         default_picker_options = {
-            'locale': {
-                'format': date_format,
-                }
+            "locale": {
+                "format": date_format,
             }
+        }
 
         if self.clearable():
-            default_picker_options['autoUpdateInput'] = False
-            default_picker_options['locale']['cancelLabel'] = _("Clear")
+            default_picker_options["autoUpdateInput"] = False
+            default_picker_options["locale"]["cancelLabel"] = _("Clear")
 
         # Rename for clarity
         picker_options = default_picker_options
         picker_options.update(self.picker_options)
 
         # If range is a dict of functions, call with 'today' as argument
-        if 'ranges' in picker_options:
-            ranges = OrderedDict(picker_options['ranges'])
+        if "ranges" in picker_options:
+            ranges = OrderedDict(picker_options["ranges"])
             for k, v in ranges.items():
                 if callable(v):
                     ranges[k] = v(datetime.today())
-            picker_options['ranges'] = ranges
+            picker_options["ranges"] = ranges
 
         # Update context for template
-        context['widget']['picker'] = {
-            'options': {
-                'json': mark_safe(json.dumps(picker_options, cls=DjangoJSONEncoder)),
-                'python': picker_options,
-                },
-            'clearable': self.clearable(),
-            'separator': self.separator,
-            }
+        context["widget"]["picker"] = {
+            "options": {
+                "json": mark_safe(json.dumps(picker_options, cls=DjangoJSONEncoder)),
+                "python": picker_options,
+            },
+            "clearable": self.clearable(),
+            "separator": self.separator,
+        }
 
         return context
 
 
 class DateTimeRangeWidget(DateRangeWidget):
-    format_key = 'DATETIME_INPUT_FORMATS'
+    format_key = "DATETIME_INPUT_FORMATS"
 
     def __init__(self, *args, **kwargs):
         super(DateTimeRangeWidget, self).__init__(*args, **kwargs)
 
-        if 'timePicker' not in self.picker_options:
-            self.picker_options['timePicker'] = True
+        if "timePicker" not in self.picker_options:
+            self.picker_options["timePicker"] = True
 
 
 class DatePickerWidget(DateRangeWidget):
     def __init__(self, *args, **kwargs):
         super(DatePickerWidget, self).__init__(*args, **kwargs)
 
-        if 'singleDatePicker' not in self.picker_options:
-            self.picker_options['singleDatePicker'] = True
+        if "singleDatePicker" not in self.picker_options:
+            self.picker_options["singleDatePicker"] = True
